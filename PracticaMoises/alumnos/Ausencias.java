640dc29908de4bd7a939d2f284ecbcd73da54407
Javier Caballero Espina <javiercaballero93@gmail.com>
2016-03-29 19:26:48 +0200

    Update and rename alumnos/Ausencias.java to PracticaMoises/alumnos/Ausencias.java

---
+++ PracticaMoises/alumnos/Ausencias.java
@@ -0,0 +1,24 @@
+package alumnos;
+
+public class Ausencias {
+	
+	private String fecha;
+	private int horas;
+	
+	public Ausencias(String fecha, int horas) {
+		
+		this.fecha = fecha;
+		this.horas = horas;
+	}
+	
+	//get necesario
+	public int getHoras() {
+		
+		return horas;
+	}
+	
+	public String mostrar_ausencias() {
+		
+		return "Día " + fecha + ", " + horas + " horas";
+	}
+}
