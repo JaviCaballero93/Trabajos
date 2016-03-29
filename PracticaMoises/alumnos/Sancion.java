faa82d47fea219457e2b681c7c48049cb4ea27d3
Javier Caballero Espina <javiercaballero93@gmail.com>
2016-03-29 19:45:11 +0200

    Update and rename alumnos/Sancion.java to PracticaMoises/alumnos/Sancion.java

--- PracticaMoises/alumnos/Sancion.java
+++ PracticaMoises/alumnos/Sancion.java
@@ -5,14 +5,14 @@ public class Sancion {
 	private String tipo_sancion;
 	private int dias_sancion;
 	
-	public Sancion(String tipo, int dias){
+	public Sancion(String tipo, int dias) {
+		
 		tipo_sancion=tipo;
 		dias_sancion=dias;
 	}
 	
-	public String mostrar_sancion(){
-		return dias_sancion + "dias de " + tipo_sancion;
+	public String mostrar_sancion() {
 		
+		return dias_sancion + "dias de " + tipo_sancion;
 	}
-
 }
